f1a728710d866961087857c61925304492dd0851
Atul-ram-3152 <atulram3152@gmail.com>
2025-06-26 01:14:54 +0530

    Add ContactButton component and integrate it into LandlordDetailPage and MyReservationsPage

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,8 @@
+
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 cursor-pointer bg-blue-600 text-white rounded-xl hover:bg-blue-800 transition">ContactButton</div>
+  )
+}
+
+export default ContactButton
